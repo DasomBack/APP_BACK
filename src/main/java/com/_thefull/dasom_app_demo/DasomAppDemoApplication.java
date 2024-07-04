fdafbaaeefb40a48848d2843193b149831ef04a6
alice1the <aliceflo014@1thefull.com>
2024-07-04 14:05:26 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/_thefull/dasom_app_demo/DasomAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com._thefull.dasom_app_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasomAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasomAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_thefull/dasom_app_demo/DasomAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com._thefull.dasom_app_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasomAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
